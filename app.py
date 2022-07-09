34d7931db406a6216b34569ff75519d11aec312e
shreyanshhub <89294432+shreyanshhub@users.noreply.github.com>
2022-07-09 17:20:33 +0530

    adding initial files

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+import youtube_dl
+import pytube
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+
+
+@app.route('/download', methods=["POST", "GET"])
+
+
+# def download():
+#	url = request.form["url"]
+#    url = pytube.YouTube(str(link.get()))
+#    video = url.streams.first()
+#    return video.download()
+
+
+
+def download():
+	url = request.form["url"]
+
+	with youtube_dl.YoutubeDL() as ydl:
+		url = ydl.extract_info(url, download=False)
+		print(url)
+		try:
+			download_link = url["entries"][-1]["formats"][-1]["url"]
+		except:
+			download_link = url["formats"][-1]["url"]
+		return redirect(download_link+"&dl=1")
+
+
+if __name__ == '__main__':
+	app.run(port=80, debug=True)
